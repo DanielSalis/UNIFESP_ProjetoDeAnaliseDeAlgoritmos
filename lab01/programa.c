d32f82d62d0032894e868b62ba3a59f847edb5f1
DanielSalis <danielsalis98@gmail.com>
2021-10-27 23:13:48 -0300

    projeto1

--- lab01/programa.c
+++ lab01/programa.c
@@ -1,65 +1,116 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
 
-#define MONTH_DAYS 30
+struct node
+{
+    int info;
+    struct node *next;
+};
+
+struct node *last = NULL;
+
+void insert(int data)
+{
+
+    struct node *temp;
+    temp = (struct node *)malloc(sizeof(struct node));
+
+    if (last == NULL)
+    {
+        temp->info = data;
+        temp->next = temp;
+        last = temp;
+    }
+
+    else
+    {
+        temp->info = data;
+        temp->next = last->next;
+        last->next = temp;
+        last = temp;
+    }
+}
+
+void update(int data)
+{
+    struct node *temp;
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp = last->next;
+    temp->info = data;
+    last = last->next;
+}
+
+void viewList()
+{
+    if (last == NULL)
+        printf("\nList is empty\n");
+
+    else
+    {
+        struct node *temp;
+        temp = last;
+        printf("ULTIMO: %d\n", temp->info);
+        printf("PRIMEIRO: %d\n", temp->next->info);
+        printf("SEGUNDO: %d\n", temp->next->next->info);
+    }
+}
+
+int up(int sum)
+{
+    if (sum % 30 == 0)
+    {
+        return (sum / 30);
+    }
+    return (sum / 30) + 1;
+}
 
 int main()
 {
+
     int inicial_users_number;
     int desired_users_number;
 
     scanf("%d", &inicial_users_number);
     scanf("%d", &desired_users_number);
 
-    int first_month_users[MONTH_DAYS];
     int new_users_counter = 0;
     int number_of_days = 0;
-    int exit = 0;
+    int mean = 0;
+    int estimated_users_counter;
 
     int i = 0;
-    for (i; i < MONTH_DAYS; i++)
+    while (1)
     {
-        scanf("%d", &first_month_users[i]);
-        new_users_counter = new_users_counter + first_month_users[i];
-
-        if (new_users_counter >= desired_users_number - inicial_users_number)
+        int qtd_users;
+        if (i < 30)
         {
-            exit = 1;
+            scanf("%d", &qtd_users);
+            insert(qtd_users);
+            new_users_counter = new_users_counter + qtd_users;
+            // mean = up(new_users_counter);
+            estimated_users_counter = new_users_counter;
+        }
+
+        else
+        {
+            int first = last->next->info;
+            mean = up(estimated_users_counter);
+            new_users_counter = new_users_counter + mean;
+            update(mean);
+            estimated_users_counter = estimated_users_counter + mean - first;
+        }
+
+        if (new_users_counter > desired_users_number - inicial_users_number)
+        {
             break;
         }
-        number_of_days = i + 1;
+
+        i++;
     }
 
-    if (exit == 0)
-    {
+    number_of_days = i;
+    printf("\n%d", number_of_days);
 
-        int mean = ceil(new_users_counter / MONTH_DAYS);
-
-        while (new_users_counter <= desired_users_number - inicial_users_number)
-        {
-
-            int aux = first_month_users[0];
-            int j = 0;
-            for (j; j < MONTH_DAYS - 1; j++)
-            {
-                first_month_users[j] = first_month_users[j + 1];
-            }
-
-            first_month_users[MONTH_DAYS-1] = mean;
-
-            new_users_counter = new_users_counter + mean;
-
-            mean = ceil((new_users_counter - aux) / 30);
-
-            i++;
-
-            printf("media: %d\n", mean);
-        }
-
-        number_of_days = i + 1;
-    }
-
-    printf("%d\n", number_of_days);
+    return 0;
 }
