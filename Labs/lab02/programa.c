ae5ce0612c4dcea334ec0bbc4624ebc8d3b1ecf9
DanielSalis <danielsalis98@gmail.com>
2021-12-03 17:13:30 -0300

    .

--- Labs/lab02/programa.c
+++ Labs/lab02/programa.c
@@ -32,16 +32,39 @@ int main()
         }
     }
 
-    printf("\n");
-    int k, l;
-    for (k = 0; k < arrays; k++)
+
+    int sum = 0;
+    for(i=0; i < iterations; i++)
     {
-        for (l = 0; l < arrays_sizes[k]; l++)
-        {
-            printf("%d ", arrays_matrix[k][l]);
+        sum =  0;
+        int minimal_value = arrays_matrix[0][0];
+        int array_sizes_position=0;
+        int j;
+        for(j=0;j < arrays; j++){
+            if(minimal_value < arrays_matrix[j][i]){
+                minimal_value = arrays_matrix[j][i];
+                array_sizes_position = j;
+            }
+            sum = sum + arrays_matrix[j][i];
         }
-        printf("\n");
+
+        for(j = 0; j < arrays_sizes[array_sizes_position] - 1; i++){
+            arrays_matrix[array_sizes_position][i] = arrays_matrix[array_sizes_position][i + 1];
+        }
     }
+
+    printf("\nSOMA: %d", sum);
+
+    // printf("\n");
+    // int k, l;
+    // for (k = 0; k < arrays; k++)
+    // {
+    //     for (l = 0; l < arrays_sizes[k]; l++)
+    //     {
+    //         printf("%d ", arrays_matrix[k][l]);
+    //     }
+    //     printf("\n");
+    // }
 
     return 0;
 }
